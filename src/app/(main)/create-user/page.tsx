847c710016a84166a5c22a21dbaef7eb7891586d
zeNn-G <treroin@gmail.com>
2024-03-07 00:09:44 +0300

    feat:create user to admin

--- src/app/(main)/create-user/page.tsx
+++ src/app/(main)/create-user/page.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { redirect } from "next/navigation";
 
 import { validateRequest } from "@/lib/validate-request";
 import { SingUpForm } from "./_components/sign-up-form";
 
-const SignupPage = async () => {
+const CreateUserPage = async () => {
   const { user } = await validateRequest();
 
-  if (user) redirect("/");
+  if (!user?.isAdmin) redirect("/");
 
   return (
     <div className="flex w-full flex-col items-center">
@@ -16,4 +17,4 @@ const SignupPage = async () => {
   );
 };
 
-export default SignupPage;
+export default CreateUserPage;

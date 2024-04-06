62b742092d0819bd71f7003c03a706a61755e20d
zeNn-G <treroin@gmail.com>
2024-04-06 21:47:02 +0300

    feat:removed mdx editor

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,9 @@
-import LiveDemo from "@/components/live-demo";
 import React from "react";
-
-import "@mdxeditor/editor/style.css";
 
 type Props = {};
 
 const MainPage = (props: Props) => {
-  return (
-    <div className="container  max-w-screen-2xl">
-      <LiveDemo />
-    </div>
-  );
+  return <div className="container max-w-screen-2xl">Home</div>;
 };
 
 export default MainPage;
--- src/components/editor/Editor.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-const MDXEditor = dynamic(
-  () => import("@mdxeditor/editor").then((mod) => mod.MDXEditor),
-  { ssr: false },
-);
-export default MDXEditor;
